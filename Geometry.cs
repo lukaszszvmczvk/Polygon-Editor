366f922d659cd80600af9c967d95445e36c9f37f
Lukasz Szymczyk <lukas0495@gmail.com>
2023-10-10 17:30:40 +0200

    New static class created

---
+++ Geometry.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    public static class Geometry
+    {
+        private static int radius = 5;
+        public static double FindDistanceToSegment(PointF pt, PointF p1, PointF p2, out PointF closest)
+        {
+            float dx = p2.X - p1.X;
+            float dy = p2.Y - p1.Y;
+            if ((dx == 0) && (dy == 0))
+            {
+                // It's a point not a line segment.
+                closest = p1;
+                dx = pt.X - p1.X;
+                dy = pt.Y - p1.Y;
+                return Math.Sqrt(dx * dx + dy * dy);
+            }
+
+            // Calculate the t that minimizes the distance.
+            float t = ((pt.X - p1.X) * dx + (pt.Y - p1.Y) * dy) /
+                (dx * dx + dy * dy);
+
+            // See if this represents one of the segment's
+            // end points or a point in the middle.
+            if (t < 0)
+            {
+                closest = new PointF(p1.X, p1.Y);
+                dx = pt.X - p1.X;
+                dy = pt.Y - p1.Y;
+            }
+            else if (t > 1)
+            {
+                closest = new PointF(p2.X, p2.Y);
+                dx = pt.X - p2.X;
+                dy = pt.Y - p2.Y;
+            }
+            else
+            {
+                closest = new PointF(p1.X + t * dx, p1.Y + t * dy);
+                dx = pt.X - closest.X;
+                dy = pt.Y - closest.Y;
+            }
+
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+        public static bool IsPointInsidePolygon(Point[] poly, Point p)
+        {
+            Point p1, p2;
+            bool inside = false;
+
+            if (poly.Length < 3)
+            {
+                return inside;
+            }
+
+            var oldPoint = new Point(
+                poly[poly.Length - 1].X, poly[poly.Length - 1].Y);
+
+            for (int i = 0; i < poly.Length; i++)
+            {
+                var newPoint = new Point(poly[i].X, poly[i].Y);
+
+                if (newPoint.X > oldPoint.X)
+                {
+                    p1 = oldPoint;
+                    p2 = newPoint;
+                }
+                else
+                {
+                    p1 = newPoint;
+                    p2 = oldPoint;
+                }
+
+                if ((newPoint.X < p.X) == (p.X <= oldPoint.X)
+                    && (p.Y - (long)p1.Y) * (p2.X - p1.X)
+                    < (p2.Y - (long)p1.Y) * (p.X - p1.X))
+                {
+                    inside = !inside;
+                }
+
+                oldPoint = newPoint;
+            }
+
+            return inside;
+        }
+        public static bool IsPointClicked(MouseEventArgs e, Point p)
+        {
+            int X = e.X;
+            int Y = e.Y;
+            return Math.Abs(X - p.X) <= radius && Math.Abs(Y - p.Y) <= radius;
+        }
+        public static bool IsEdgeClicked(MouseEventArgs e, Edge edge)
+        {
+            var closest = new PointF();
+            return FindDistanceToSegment(new Point(e.X, e.Y), edge.p1, edge.p2, out closest) <= radius;
+        }
+    }
+}
--- PolygonCanvas.cs
+++ PolygonCanvas.cs
@@ -143,7 +143,7 @@ namespace lab1
             if(Points.Count<2)
                 return false;
             
-            if (IsPointClicked(e, Points[0]))
+            if (Geometry.IsPointClicked(e, Points[0]))
             {
                 SolidBrush brush = new SolidBrush(Color.FromArgb(128, 0, 0, 0));
                 Polygons.Add(new Polygon(Points));
@@ -167,20 +167,20 @@ namespace lab1
             {
                 for(int j = 0; j < poly.Points.Count; ++j)
                 {
-                    if (IsPointClicked(e, poly.Points[j]))
+                    if (Geometry.IsPointClicked(e, poly.Points[j]))
                     {
                         selectedPolygon = poly;
                         selectedPoint = poly.Points[j];
                         return true;
                     }
-                    if(j>=1 && IsEdgeClicked(e,new Edge(poly.Points[j], poly.Points[j-1])))
+                    if(j>=1 && Geometry.IsEdgeClicked(e,new Edge(poly.Points[j], poly.Points[j-1])))
                     {
                         selectedPolygon = poly;
                         selectedEdge = new Edge(poly.Points[j-1], poly.Points[j]);
                         return true;
                     }
                 }
-                if (IsEdgeClicked(e, new Edge(poly.Points[poly.Points.Count-1], poly.Points[0])))
+                if (Geometry.IsEdgeClicked(e, new Edge(poly.Points[poly.Points.Count-1], poly.Points[0])))
                 {
                     selectedPolygon = poly;
                     selectedEdge = new Edge(poly.Points[poly.Points.Count-1], poly.Points[0]);
@@ -193,7 +193,7 @@ namespace lab1
         {
             for (int i = 0; i < Polygons.Count; ++i)
             {
-                if (IsPointInsidePolygon(Polygons[i].Points.ToArray(), new Point(e.X, e.Y)))
+                if (Geometry.IsPointInsidePolygon(Polygons[i].Points.ToArray(), new Point(e.X, e.Y)))
                 {
                     selectedPolygon = Polygons[i];
                     return;
@@ -206,7 +206,7 @@ namespace lab1
             {
                 for(int i=0; i<poly.Points.Count; ++i)
                 {
-                    if(IsPointClicked(e, poly.Points[i]))
+                    if(Geometry.IsPointClicked(e, poly.Points[i]))
                     {
                         poly.Points.RemoveAt(i);
                         if(poly.Points.Count == 0)
@@ -214,7 +214,7 @@ namespace lab1
                         DrawPolygons();
                         return true;
                     }
-                    else if(i>0 && IsEdgeClicked(e, new Edge(poly.Points[i - 1], poly.Points[i])))
+                    else if(i>0 && Geometry.IsEdgeClicked(e, new Edge(poly.Points[i - 1], poly.Points[i])))
                     {
                         poly.Points.RemoveAt(i);
                         poly.Points.RemoveAt(i-1);
@@ -224,7 +224,7 @@ namespace lab1
                         return true;
                     }
                 }
-                if(IsEdgeClicked(e, new Edge(poly.Points[poly.Points.Count - 1], poly.Points[0])))
+                if(Geometry.IsEdgeClicked(e, new Edge(poly.Points[poly.Points.Count - 1], poly.Points[0])))
                 {
                     poly.Points.RemoveAt(0);
                     poly.Points.RemoveAt(poly.Points.Count - 1);
@@ -241,53 +241,13 @@ namespace lab1
             var p = new Point(e.X, e.Y);
             foreach (var poly in Polygons)
             {
-                if (IsPointInsidePolygon(poly.Points.ToArray(), p))
+                if (Geometry.IsPointInsidePolygon(poly.Points.ToArray(), p))
                 {
                     Polygons.Remove(poly);
                     DrawPolygons();
                     return;
                 }
             }
-        }
-        private double FindDistanceToSegment(PointF pt, PointF p1, PointF p2, out PointF closest)
-        {
-            float dx = p2.X - p1.X;
-            float dy = p2.Y - p1.Y;
-            if ((dx == 0) && (dy == 0))
-            {
-                // It's a point not a line segment.
-                closest = p1;
-                dx = pt.X - p1.X;
-                dy = pt.Y - p1.Y;
-                return Math.Sqrt(dx * dx + dy * dy);
-            }
-
-            // Calculate the t that minimizes the distance.
-            float t = ((pt.X - p1.X) * dx + (pt.Y - p1.Y) * dy) /
-                (dx * dx + dy * dy);
-
-            // See if this represents one of the segment's
-            // end points or a point in the middle.
-            if (t < 0)
-            {
-                closest = new PointF(p1.X, p1.Y);
-                dx = pt.X - p1.X;
-                dy = pt.Y - p1.Y;
-            }
-            else if (t > 1)
-            {
-                closest = new PointF(p2.X, p2.Y);
-                dx = pt.X - p2.X;
-                dy = pt.Y - p2.Y;
-            }
-            else
-            {
-                closest = new PointF(p1.X + t * dx, p1.Y + t * dy);
-                dx = pt.X - closest.X;
-                dy = pt.Y - closest.Y;
-            }
-
-            return Math.Sqrt(dx * dx + dy * dy);
         }
         private void DrawLine(Point p1, Point p2)
         {
@@ -320,60 +280,9 @@ namespace lab1
                 }
             }
         }
-        private bool IsPointInsidePolygon(Point[] poly, Point p)
-        {
-            Point p1, p2;
-            bool inside = false;
-
-            if (poly.Length < 3)
-            {
-                return inside;
-            }
-
-            var oldPoint = new Point(
-                poly[poly.Length - 1].X, poly[poly.Length - 1].Y);
-
-            for (int i = 0; i < poly.Length; i++)
-            {
-                var newPoint = new Point(poly[i].X, poly[i].Y);
-
-                if (newPoint.X > oldPoint.X)
-                {
-                    p1 = oldPoint;
-                    p2 = newPoint;
-                }
-                else
-                {
-                    p1 = newPoint;
-                    p2 = oldPoint;
-                }
-
-                if ((newPoint.X < p.X) == (p.X <= oldPoint.X)
-                    && (p.Y - (long)p1.Y) * (p2.X - p1.X)
-                    < (p2.Y - (long)p1.Y) * (p.X - p1.X))
-                {
-                    inside = !inside;
-                }
-
-                oldPoint = newPoint;
-            }
-
-            return inside;
-        }
         private int mod(int x, int m)
         {
             return (x % m + m) % m;
-        }
-        private bool IsPointClicked(MouseEventArgs e, Point p)
-        {
-            int X = e.X;
-            int Y = e.Y;
-            return Math.Abs(X - p.X) <= radius && Math.Abs(Y - p.Y) <= radius;
-        }
-        private bool IsEdgeClicked(MouseEventArgs e, Edge edge) 
-        {
-            var closest = new PointF();
-            return FindDistanceToSegment(new Point(e.X,e.Y), edge.p1, edge.p2, out closest) <= radius;
         }
         public void Clear()
         {
